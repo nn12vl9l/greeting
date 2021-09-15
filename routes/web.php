be5d89fbf7251dd3a42183e733b01d8d9987524e
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-09-15 14:02:08 +0900

    課題修正

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'App\Http\Controllers\ItemController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/comments/random', [App\Http\Controllers\MessageController::class, 'random']);
 Route::get('/comments/{time}', [App\Http\Controllers\MessageController::class, 'greeting']);
